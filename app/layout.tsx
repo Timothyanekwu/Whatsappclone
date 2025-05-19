59e292e3899687af938a5be18326fed2b98de29b
Noax89 <moshoodqudus89@gmail.com>
2025-05-19 23:01:00 +0100

    checking theme updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {Inter} from "next/font/google";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: '--font-inter',
+});
+
+export const metadata: Metadata = {
+  title: "Whatsapp",
+  description: "A real-time chat application",
+};
 
 export default function RootLayout({
   children,
@@ -8,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" >
+      <body className={`${inter.className} bg-primaryDark`}>
         {children}
       </body>
     </html>
