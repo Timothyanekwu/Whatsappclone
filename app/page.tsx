81c989e5b3f4aa8faca487838f3518ee7bba3fa9
Noax89 <moshoodqudus89@gmail.com>
2025-05-20 00:21:39 +0100

    checking the theme updates 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-primaryDark flex items-center justify-center ">
+    <div className="w-full h-screen bg-secondary flex items-center justify-center ">
         <p className="text-5xl text-black font-black">Whatsapp Clone</p>
     </div>
   );
