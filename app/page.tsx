98e3794d97f7754090bd5815355d0d513c539825
Noax89 <moshoodqudus89@gmail.com>
2025-05-19 12:55:11 +0100

    commit1

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center ">
+        <p className="text-5xl font-bold text-black">Whatsapp Clone</p>
+    </div>
+  );
+}
